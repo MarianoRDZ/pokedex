2f65c9266e4860e26baacf9b6ffe0e76e5c22089
Mariano Rodriguez <rdzc.mariano@gmail.com>
2025-07-03 13:54:46 -0300

    feat: Added prettier to ESLint and added more scripts to package.json

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,22 @@
 module.exports = {
   root: true,
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["eslint:recommended", "plugin:react/recommended", "react-app"],
+  env: { browser: true, es2021: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+  ],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaFeatures: { jsx: true },
   },
-  plugins: ["react"],
+  plugins: ["react", "jsx-a11y", "prettier"],
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
+    "prettier/prettier": ["error"],
   },
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  settings: { react: { version: "detect" } },
 };
