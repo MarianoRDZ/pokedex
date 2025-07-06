dcc823a2ad0a129fe1cd7c7452d32446c918cb46
Mariano Rodriguez <rdzc.mariano@gmail.com>
2025-07-06 15:08:41 -0300

    chore: prepare eslint to add jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,27 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2021: true },
+  env: { browser: true, es2021: true, jest: true },
   extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:jsx-a11y/recommended",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
+    ecmaVersion: latest,
+    sourceType: 'module',
     ecmaFeatures: { jsx: true },
   },
-  plugins: ["react", "jsx-a11y", "prettier"],
+  plugins: ['react', 'jsx-a11y', 'prettier'],
   rules: {
-    "react/react-in-jsx-scope": "off",
-    "react/prop-types": "off",
-    "prettier/prettier": ["error"],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
-  settings: { react: { version: "detect" } },
+  settings: { react: { version: 'detect' } },
 };
