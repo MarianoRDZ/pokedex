c9ead5609cce103d683b766890364284d476a066
Mariano Rodriguez <rdzc.mariano@gmail.com>
2025-07-07 00:11:15 -0300

    feat: add a constant file with pokemon types and matching colors

---
+++ src/constants/TYPES.js
@@ -0,0 +1,64 @@
+const TYPES = {
+  NORMAL: {
+    name: 'normal',
+    color: '#A8A878',
+  },
+  FIRE: {
+    name: 'fire',
+    color: '#F08030',
+  },
+  WATER: {
+    name: 'water',
+    color: '#6890F0',
+  },
+  GRASS: {
+    name: 'grass',
+    color: '#78C850',
+  },
+  ELECTRIC: {
+    name: 'electric',
+    color: '#F8D030',
+  },
+  ICE: {
+    name: 'ice',
+    color: '#98D8D8',
+  },
+  FIGHTING: {
+    name: 'fighting',
+    color: '#C03028',
+  },
+  POISON: {
+    name: 'poison',
+    color: '#A040A0',
+  },
+  GROUND: {
+    name: 'ground',
+    color: '#E0C068',
+  },
+  FLYING: {
+    name: 'flying',
+    color: '#A890F0',
+  },
+  PSYCHIC: {
+    name: 'psychic',
+    color: '#F85888',
+  },
+  BUG: {
+    name: 'bug',
+    color: '#A8B820',
+  },
+  ROCK: {
+    name: 'rock',
+    color: '#B8A038',
+  },
+  GHOST: {
+    name: 'ghost',
+    color: '#705898',
+  },
+  DRAGON: {
+    name: 'dragon',
+    color: '#7038F8',
+  },
+};
+
+export default TYPES;
