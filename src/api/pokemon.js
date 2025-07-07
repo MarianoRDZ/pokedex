aa8178577c53cbab8d5745ca3b5badb4d1e178fa
Mariano Rodriguez <rdzc.mariano@gmail.com>
2025-07-07 00:09:31 -0300

    feat: add api to search pokemon by name

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -14,3 +14,16 @@ export const getPokemonList = async (limit) => {
 
   return normalizedData;
 };
+
+export const getPokemonByName = async (name) => {
+  const response = await API.get(`${ENDPOINT}/${name}`);
+
+  return {
+    name: response.data.name,
+    id: response.data.id,
+    types: response.data.types,
+    height: response.data.height,
+    weight: response.data.weight,
+    sprite: response.data.sprites.front_default,
+  };
+};
