eb249a7a02e9963c0977b35fe41f33d2576f364c
Mariano Rodriguez <rdzc.mariano@gmail.com>
2025-07-14 16:29:01 -0300

    fix: import warning

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ const PokemonModal = React.lazy(
   () => import('./components/Modal/PokemonModal.jsx')
 );
 import Header from './components/Header/Header.jsx';
-import { GlobalStyles } from './styles/GlobalStyles';
 import { Container } from './App.styles.js';
 
 function App() {
   return (
     <Container>
       <Header />
-      <GlobalStyles />
       <List />
       <PokemonModal />
     </Container>
