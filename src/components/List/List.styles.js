13710ed0ec00112add30cc0269934fb1621e8cc8
Mariano Rodriguez <rdzc.mariano@gmail.com>
2025-07-17 15:53:21 -0300

    style: center cards

--- src/components/List/List.styles.js
+++ src/components/List/List.styles.js
@@ -4,9 +4,10 @@ export const PokemonList = styled.div`
   display: grid;
   gap: 1rem;
   padding: 1rem;
-  grid-template-columns: repeat(auto-fit, minmax(160px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
   margin: 0 auto;
   width: 100%;
   max-width: 1440px;
   background: #eff2fe;
+  justify-items: center;
 `;
