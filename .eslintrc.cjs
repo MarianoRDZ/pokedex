ced89d56a5dec046d0917616d1a6ad68f78c503d
Mariano Rodriguez <rdzc.mariano@gmail.com>
2025-07-17 03:31:16 -0300

    feat: add github pages config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,7 @@ module.exports = {
     'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: latest,
+    ecmaVersion: 'latest',
     sourceType: 'module',
     ecmaFeatures: { jsx: true },
   },
