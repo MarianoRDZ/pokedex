1282e4c0c6c2da4e48c99229319627b660c91b1e
Mariano Rodriguez <rdzc.mariano@gmail.com>
2025-07-17 01:25:16 -0300

    tests: updated Header tests

--- src/components/Header/Header.test.jsx
+++ src/components/Header/Header.test.jsx
@@ -3,6 +3,7 @@ import { describe, it, expect } from 'vitest';
 import Header from './Header';
 import { Provider } from 'react-redux';
 import configureStore from 'redux-mock-store';
+import Searchbar from '../Searchbar/Searchbar';
 
 const mockStore = configureStore([]);
 const store = mockStore({});
@@ -11,11 +12,19 @@ describe('Header', () => {
   it('should render the title and subtitle', () => {
     render(
       <Provider store={store}>
-        <Header />
+        <Header>
+          <Header.Title>Pokedex</Header.Title>
+          <Header.Subtitle>
+            Descubrí los 151 pokemon originales!
+          </Header.Subtitle>
+          <Header.Searchbar>
+            <Searchbar />
+          </Header.Searchbar>
+        </Header>
       </Provider>
     );
 
-    expect(screen.getByText('Pokédex')).toBeInTheDocument();
+    expect(screen.getByText('Pokedex')).toBeInTheDocument();
     expect(
       screen.getByText('Descubrí los 151 pokemon originales!')
     ).toBeInTheDocument();
@@ -24,7 +33,15 @@ describe('Header', () => {
   it('should render the Searchbar component', () => {
     render(
       <Provider store={store}>
-        <Header />
+        <Header>
+          <Header.Title>Pokedex</Header.Title>
+          <Header.Subtitle>
+            Descubrí los 151 pokemon originales!
+          </Header.Subtitle>
+          <Header.Searchbar>
+            <Searchbar />
+          </Header.Searchbar>
+        </Header>
       </Provider>
     );
 
